f686a2fd2d40016f29c09b735305489073e8283c
Junaid Khan <dr.khanjunaid@gmail.com>
2025-01-04 22:34:23 +0530

    new salah add feature and salah timing edit column

--- app/controllers/salah_timings_controller.rb
+++ app/controllers/salah_timings_controller.rb
@@ -1,7 +1,23 @@
 class SalahTimingsController < ApplicationController
   before_action :authenticate_user!
 
-  def edit
+  def new
+    @mosque = Mosque.find(params[:mosque_id])
+    @salah_timing = SalahTiming.new
+  end
+  
+  def create
+    @mosque = Mosque.find(params[:mosque_id])
+    @salah_timing = SalahTiming.new(salah_timing_params)
+    @salah_timing.mosque_id = @mosque.id
+    if @salah_timing.save
+      redirect_to mosque_path(@mosque.id), notice: "salah added successfully."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+    def edit
     @mosque = Mosque.find(params[:mosque_id])
     @salah_timing = SalahTiming.find(params[:id])
   end
