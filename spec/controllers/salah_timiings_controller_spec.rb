ea8afd076d72e98247f235eeecd0c8e432abf2b4
Junaid Khan <dr.khanjunaid@gmail.com>
2025-01-11 08:37:23 +0530

    test for salah timing

--- spec/controllers/salah_timiings_controller_spec.rb
+++ spec/controllers/salah_timiings_controller_spec.rb
@@ -1,13 +1,43 @@
-Rspec.describe SalahTimingsController, type: :controller do
-  context "edit" do
-  it 'it renders edit page' do
-    sign_in(user)
-    name = create(:name, salah_name: "Fajar")
-    salah_timing = create(:salah_timing, salah_timing: "06.40", name:)
+RSpec.describe SalahTimingsController, type: :controller do
+  let(:user) { create(:user) }
   
-    get :edit, params: { id: mosque.id }
-    expect(response)
+ context "new" do
+    it 'renders new page form' do 
+      sign_in(user)
+      mosque = create(:mosque, user:)
+
+      get :new, params: {mosque_id:mosque.id}
+
+      expect(response).to have_http_status(:ok)
+      expect(response).to render_template('new')
+    end
   end
+
+  context "create" do
+    fit "successfully add new salah request" do
+      sign_in(user)
+      mosque = create(:mosque, user:)
+
+      post :create, params: {mosque_id: mosque.id, salah_timing: {name: "fajr", timing:"6:40"} }
+
+      expect(response).to have_http_status(302)
+
+      expect(SalahTiming.last.name).to eq("fajr")
+      expect(SalahTiming.last.timing).to eq("6:40")
+    end
+    
   end
+
+  context "edit" do
+    it 'it renders edit page' do
+      sign_in(user)
+      mosque = create(:mosque, user:)
+      salah_timing = create(:salah_timing, :fajr, mosque:)
+  
+      get :edit, params: { mosque_id: mosque.id, id: salah_timing.id}
+      expect(response).to have_http_status(:ok)
+      expect(response).to render_template('edit')
+    end
   end
+end
   
