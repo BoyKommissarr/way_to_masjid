e70070d351bcce62b4f43c1ebab9fac29202f9cb
Abid Shaikh <shaikhabid2104@gmail.com>
2025-01-25 08:23:52 +0530

    Minor Commit for super admin

--- app/controllers/admin/mosques_controller.rb
+++ app/controllers/admin/mosques_controller.rb
@@ -3,17 +3,17 @@ module Admin
     before_action :authenticate_user!
     authorize_resource
 
-    layout 'admin'
+    layout "admin"
 
     def index
       @mosques = Mosque.all
-      @type = 'all'
-      if params[:status] == 'pending'
+      @type = "all"
+      if params[:status] == "pending"
         @mosques = Mosque.pending
-        @type = 'pending'
-      elsif params[:status] == 'approved'
+        @type = "pending"
+      elsif params[:status] == "approved"
         @mosques = Mosque.approved
-        @type = 'approved'
+        @type = "approved"
       end
     end
   end
